8b3007e0a94347cb8726c7f2030e85c3e48ebd89
bartr <bartr@microsoft.com>
2026-04-23 15:48:50 -0500

    updated version

--- cllm/internal/buildinfo/version.go
+++ cllm/internal/buildinfo/version.go
@@ -2,4 +2,4 @@ package buildinfo
 
 // Version is the application version. It defaults to the checked-in release
 // value and can be overridden at build time with -ldflags -X.
-var Version = "0.1.0"
+var Version = "0.2.0"
