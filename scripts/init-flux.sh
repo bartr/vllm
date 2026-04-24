27a4e784636558f32587afa9b23f5f96a4218a1f
bartr <bartr@microsoft.com>
2026-04-23 20:34:09 -0500

    updated GitOps

---
+++ scripts/init-flux.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+cd "$script_dir" || exit 1
+
+cd ../clusters/z01/flux-system
+kubectl apply -f components.yaml
+kubectl apply -f source.yaml
+kubectl wait --namespace flux-system --for=condition=Ready pod --all --timeout=60s
+
+kubectl apply -f listeners/nvidia-plugin.yaml
+flux reconcile kustomization nvidia-plugin
+
+kubectl apply -f listeners/vllm.yaml
+flux reconcile kustomization vllm
+
+kubectl apply -f listeners/traefik.yaml
+flux reconcile kustomization traefik
+
+echo ''
+echo 'Waiting for Prometheus operator to start - this can take up to 10 minutes'
+echo ''
+kubectl apply -f listeners/prometheus-operator.yaml
+flux reconcile kustomization prometheus-operator
+kubectl wait --namespace monitoring --for=condition=Ready pod --all --timeout=10m
+
+echo ''
+echo 'Waiting for Prometheus to start - this can take up to 5 minutes'
+echo ''
+kubectl apply -f listeners/prometheus.yaml
+flux reconcile kustomization prometheus
+kubectl wait --namespace monitoring --for=condition=Ready pod --all --timeout=5m
+
+kubectl apply -f listeners/dcgm-exporter.yaml
+flux reconcile kustomization dcgm-exporter
+
+kubectl apply -f listeners/grafana.yaml
+flux reconcile kustomization grafana
+echo ''
+echo 'Waiting for Grafana to start - this can take up to 5 minutes'
+echo ''
+kubectl wait --namespace monitoring --for=condition=Ready pod --all --timeout=5m
+
+echo ''
+echo 'Waiting for vLLM pod to start - this can take up to 15 minutes'
+echo ''
+kubectl wait --namespace vllm --for=condition=Ready pod --all --timeout=15m
+
+kubectl apply -f flux-listeners.yaml
+flux reconcile kustomization flux-listeners
+
+kubectl wait --namespace monitoring --for=condition=Ready pod --all --timeout=5m
+kubectl wait --namespace cllm --for=condition=Ready pod --all --timeout=5m
