06adca33d5a2592adeaded7e51c2a779208efdd7
bartr <bartr@microsoft.com>
2026-04-23 20:17:56 -0500

    updated GitOps

--- scripts/reset-k3s.sh
+++ scripts/reset-k3s.sh
@@ -23,10 +23,29 @@ kubectl apply -f components.yaml
 kubectl apply -f source.yaml
 kubectl wait --namespace flux-system --for=condition=Ready pod --all --timeout=60s
 
-kubectl apply -f listeners/nvidia-device-plugin.yaml
-flux reconcile kustomization nvidia-plugin
-kubectl wait --namespace nvidia-device-plugin --for=condition=Ready pod --all --timeout=300s
-
 kubectl apply -f listeners/vllm.yaml
 flux reconcile kustomization vllm
+
+kubectl apply -f listeners/nvidia-plugin.yaml
+flux reconcile kustomization nvidia-plugin
+
+kubectl apply -f listeners/traefik.yaml
+flux reconcile kustomization traefik
+
+kubectl apply -f listeners/prometheus-operator.yaml
+flux reconcile kustomization prometheus-operator
+
+kubectl apply -f listeners/prometheus.yaml
+flux reconcile kustomization prometheus
+kubectl wait --namespace monitoring --for=condition=Ready pod --all --timeout=5m
+
+echo ''
+echo 'Waiting for vLLM pod to start - this can take up to 15 minutes'
+echo ''
 kubectl wait --namespace vllm --for=condition=Ready pod --all --timeout=15m
+
+kubectl apply -f flux-listeners.yaml
+flux reconcile kustomization flux-listeners
+
+kubectl wait --namespace monitoring --for=condition=Ready pod --all --timeout=5m
+kubectl wait --namespace cllm --for=condition=Ready pod --all --timeout=5m
