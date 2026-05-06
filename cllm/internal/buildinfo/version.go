9bf8b5b56a2b5575698db65d7f39c4b3bf5828ee
bartr <bartr@microsoft.com>
2026-05-05 22:36:57 -0500

    bump dev version to 0.20.0

--- cllm/internal/buildinfo/version.go
+++ cllm/internal/buildinfo/version.go
@@ -2,4 +2,4 @@ package buildinfo
 
 // Version is the application version. It defaults to the checked-in release
 // value and can be overridden at build time with -ldflags -X.
-var Version = "0.19.0"
+var Version = "0.20.0"
